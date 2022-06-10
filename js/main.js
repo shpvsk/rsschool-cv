11714c4e9a7587fe8d0b477c0d1d401db767e918
shpvsk <mshpvsk@yandex.ru>
2022-06-10 14:27:16 +0300

    feat: logic for burger menu

--- js/main.js
+++ js/main.js
@@ -27,3 +27,43 @@ navLinks.forEach(link => {
 	link.addEventListener('click', scroll)
 })
 
+
+// burger menu
+if (document.body.clientWidth < 992) {
+
+	const $burgerMenu = selector =>  {
+		const menu = document.querySelector(selector)
+		const btn = menu.querySelector('.burger-menu__btn');
+		const links = menu.querySelectorAll('.nav-list > li a');
+		const overlay = menu.querySelector('.burger-menu__overlay');
+
+		btn.addEventListener('click', e => {
+			$toggleMenu()
+			e.preventDefault()
+		})
+
+		links.forEach(i => {
+			i.addEventListener('click', e => {
+				e.preventDefault()
+				$toggleMenu()
+			})
+		})
+
+		overlay.addEventListener('click', () => {
+			$toggleMenu()
+		})
+
+		const $toggleMenu = () => {
+			if (!menu.classList.contains('burger-menu_active')) {
+				menu.classList.add('burger-menu_active')
+				btn.classList.add('burger-menu__btn_active')
+				document.body.classList.add('body_fixed')
+			} else {
+				menu.classList.remove('burger-menu_active')
+				btn.classList.remove('burger-menu__btn_active')
+				document.body.classList.remove('body_fixed')
+			}
+		}
+	}
+	$burgerMenu('.burger-menu')
+}
